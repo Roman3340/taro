6d6bb6228000c0f2cb054de874e71b86c8abf14b
Roman3340 <160294762+Roman3340@users.noreply.github.com>
2024-10-21 21:30:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const tg = window.Telegram.WebApp; // Инициализация объекта Telegram WebApp
+
+tg.ready(function () { // Дожидаемся полной инициализации приложения
+    console.log("Telegram WebApp готово");
+
+    const cardImages = [
+        "images/RWS_Tarot_00_Fool.jpg",
+        "images/RWS_Tarot_01_Magician.jpg",
+        "images/RWS_Tarot_02_High_Priestess.jpg",
+        "images/RWS_Tarot_03_Empress.jpg",
+        "images/RWS_Tarot_04_Emperor.jpg",
+        "images/RWS_Tarot_05_Hierophant.jpg"
+    ];
+
+    const maxFlippedCards = 3;
+    let flippedCardsCount = 0;
+    let selectedCards = [];
+
+    // Функция для получения случайной карты
+    function getRandomCard() {
+        const randomIndex = Math.floor(Math.random() * cardImages.length);
+        const selectedCard = cardImages[randomIndex];
+        cardImages.splice(randomIndex, 1); // Удаляем карту из массива
+        return selectedCard;
+    }
+
+    // Функция переворота карты
+    function flipCard(cardElement) {
+        const card = cardElement.querySelector('.card');
+        if (card.classList.contains('flipped') || flippedCardsCount >= maxFlippedCards) {
+            return;
+        }
+
+        flippedCardsCount += 1;
+        const frontFace = cardElement.querySelector('.card-front img');
+        if (!frontFace) {
+            const imgElement = document.createElement('img');
+            const randomCard = getRandomCard();
+            imgElement.src = randomCard;
+            selectedCards.push(randomCard); // Добавляем выбранную карту в массив
+            cardElement.querySelector('.card-front').appendChild(imgElement);
+        }
+
+        card.classList.add('flipped');
+
+        // Показываем MainButton от Telegram, если выбрано 3 карты
+        if (flippedCardsCount === maxFlippedCards) {
+            tg.MainButton.setText("Далее");
+            tg.MainButton.show(); // Показываем MainButton
+        }
+    }
+
+    // Привязываем функцию переворота к каждой карте на странице
+    document.querySelectorAll('.main-image').forEach(function (element) {
+        element.addEventListener('click', function () {
+            flipCard(this);
+        });
+    });
+
+    // Функция для отправки данных о выбранных картах
+    tg.MainButton.onClick(function () {
+        const selectedCardNames = selectedCards.map(card => card.split('_')[2].replace('.jpg', ''));
+
+        // Отправляем данные в Telegram
+        tg.sendData(JSON.stringify(selectedCardNames));
+
+        // Закрываем веб-приложение
+        tg.close();
+    });
+});
